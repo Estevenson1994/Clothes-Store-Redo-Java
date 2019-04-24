fcef10857bd5317864b9e8ca06bf882802c4482e
Erin Stevenson <estevenson_94@live.com>
2019-04-24 13:08:16 +0100

    can return all items from json

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,3 +1,11 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.io.FileUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
 public class Product {
     public int id;
     public String name;
@@ -6,4 +14,22 @@ public class Product {
         this.id = id;
         this.name = name;
     }
+
+    public static List<Product> allProducts() throws Exception {
+        List<Product> products = new ArrayList<>();
+
+        File file = new File("./production_data.json");
+        String content = FileUtils.readFileToString(file, "utf-8");
+
+        JSONObject obj = new JSONObject(content);
+
+        JSONArray arr = obj.getJSONArray("products");
+        for (int i = 0; i < arr.length(); i++) {
+            String name  = arr.getJSONObject(i).getString("name");
+            products.add(new Product(i, name));
+        }
+        return products;
+    }
+
+
 }
--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -1,11 +1,14 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class ProductTest {
 
     public Product shoes;
+    public List<Product> products;
 
     @Before
     public void beforeEachTestMethod() {
@@ -18,6 +21,26 @@ public class ProductTest {
         assertEquals("Almond Court Shoe", shoes.name);
     }
 
+    @Test
+    public void testReturnAllProductsHasLength13() throws Exception {
+        products = Product.allProducts();
+        assertEquals(13, products.size());
+    }
+
+    @Test
+    public void testFirstProductHasCorrectName() throws Exception {
+        Product first = Product.allProducts().get(0);
+        assertEquals(0, first.id);
+        assertEquals("Almond Toe Court Shoes", first.name);
+
+    }
+
+    @Test
+    public void testLastProductHasCorrectName() throws Exception {
+        Product last = Product.allProducts().get(12);
+        assertEquals(12, last.id);
+        assertEquals("Mid Twist Cut-Out Dress", last.name);
+    }
 
 
 }
